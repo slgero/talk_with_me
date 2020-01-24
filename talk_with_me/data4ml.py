d371b4499a73528cf953d455311a623a6df7095c
root <sersavvov@yandex.ru>
2020-01-24 12:10:49 +0300

    Test black formatter 2

--- talk_with_me/data4ml.py
+++ talk_with_me/data4ml.py
@@ -6,41 +6,57 @@ import re
 
 
 class Data4ML:
-    def __init__(self, path_to_config='./data_params.json'):
+    def __init__(self, path_to_config="./data_params.json"):
         cfg = self.read_json(path_to_config)
-        self.blacklist = ['Фотография', 'Аудиозапись', 'Документ', 'Видеозапись', 'прикреплённое сообщение', 'Запись на стене']
-    
+        self.blacklist = [
+            "Фотография",
+            "Аудиозапись",
+            "Документ",
+            "Видеозапись",
+            "прикреплённое сообщение",
+            "Запись на стене",
+        ]
+
     def get_list_of_files_in_folder(self, folder_name: str) -> list:
-        
+
         if os.path.isdir(folder_name):
             # Get list of only html files from folder:
             files = [file for file in listdir(folder_name) if file.endswith(".html")]
-            
+
             # Descending sort to consider message order:
-            files = sorted(files, key=lambda x: int(re.search('messages(\d+)\.html', x).group(1)), reverse=True)
+            files = sorted(
+                files,
+                key=lambda x: int(re.search("messages(\d+)\.html", x).group(1)),
+                reverse=True,
+            )
             return files
         else:
-            print(f'No such directory: {folder_name}')
+            print(f"No such directory: {folder_name}")
             return []
 
-    
     def parse_html(self, folder: str, files: list):
         all_messages = []
-        
-        for file in files:
-            with open(join(folder, file), 'r') as f:
-                soup = BeautifulSoup(f, 'lxml')
-                
-            messages = []
-            for message in soup.find_all('div', {'class': 'meassage'}):
-                message = message.text.strip()
-    
-    def clear_message(self, messages: list) -> list:
-        blacklist = ['Фотография', 'Документ', 'Видеозапись']
-        message_ends = ['прикреплённое сообщение', 'прикреплённых сообщений', 'прикреплённых сообщения', 'Запись на стене', 'Сообщение удалено']
 
-        perfect_url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-        perfect_emoji_regex = r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])'
+        for file in files:
+            with open(join(folder, file), "r") as f:
+                soup = BeautifulSoup(f, "lxml")
+
+            messages = []
+            for message in soup.find_all("div", {"class": "meassage"}):
+                message = message.text.strip()
+
+    def clear_message(self, messages: list) -> list:
+        blacklist = ["Фотография", "Документ", "Видеозапись"]
+        message_ends = [
+            "прикреплённое сообщение",
+            "прикреплённых сообщений",
+            "прикреплённых сообщения",
+            "Запись на стене",
+            "Сообщение удалено",
+        ]
+
+        perfect_url_regex = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+        perfect_emoji_regex = r"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])"
 
         clean_messages = []
         for i in messages:
@@ -48,27 +64,26 @@ class Data4ML:
             # TODO: add it into list
             for end in message_ends:
                 if i.endswith(end):
-                    i = i[:i.rfind('\n')]
+                    i = i[: i.rfind("\n")]
 
             # If URL in message - not append this message:
-            if '\nСсылка\nhttps:' in i:
+            if "\nСсылка\nhttps:" in i:
                 continue
 
             # delet url:
             for attachment in blacklist:
-                i = re.sub(f'\n{attachment}\n'+perfect_url_regex, '', i)
+                i = re.sub(f"\n{attachment}\n" + perfect_url_regex, "", i)
 
             # delete trash after emoji:
-            i = re.sub(perfect_emoji_regex, '', i)
-            
+            i = re.sub(perfect_emoji_regex, "", i)
+
             i = i.strip()
             if i:
                 clean_messages.append(i)
         return clean_messages
 
-    
     @staticmethod
     def read_json(path_to_config: str) -> dict:
-        with open(path_to_config, 'r') as f:
+        with open(path_to_config, "r") as f:
             cfg = json.load(f)
         return cfg
--- talk_with_me/test.py
+++ talk_with_me/test.py
@@ -5,16 +5,16 @@ from data4ml import Data4ML
 class TestData4ML(unittest.TestCase):
     def setUp(self):
         self.data4ml = Data4ML()
-        
+
     def test_clear_message(self):
         samples = {
-            "Привет, как дела?" : "Привет, как дела?",
-            "   Привет, как дела?   " : "Привет, как дела?"
+            "Привет, как дела?": "Привет, как дела?",
+            "   Привет, как дела?   ": "Привет, как дела?",
         }
-        
+
         for key, value in samples.items():
             self.assertEqual(self.data4ml.clear_message([key]), [value])
-            
-            
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
