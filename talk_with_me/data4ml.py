50a1434862a1d3d8b5d08efd3c3d16fb78ae3c1f
GitHub Action <action@github.com>
2020-01-30 16:42:00 +0000

    ci: Automated build push

--- talk_with_me/data4ml.py
+++ talk_with_me/data4ml.py
@@ -49,7 +49,7 @@ class Data4ML(ABC):
                 if len(folder) == 10:  # group chats
                     continue
                 folders.append(folder)
-                
+
                 # This is so that the folders are in the same order as on the git:
                 folders.sort()
         else:
